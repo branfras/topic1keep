f6146fe618d2f3e6753b85b38200d029a9fa4450
Brandon Fraser <FrasBran034@GOTVDSB.CA>
2022-09-22 09:16:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace topic1keep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Brandon";
+            string favMovie = "The Matrix";
+            string movQuote = "You shall not pass";
+            Console.WriteLine($"greetings {firstName.ToLower()}. your favourite movie is {favMovie.ToLower()}.");
+            favMovie = favMovie.Replace(favMovie, favMovie.ToUpper());
+            Console.WriteLine(favMovie);
+            Console.WriteLine(favMovie.Contains("THE"));
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+            Console.WriteLine(favMovie);
+            Console.WriteLine(" ");
+            movQuote = movQuote.Replace("o", " ");
+            movQuote = movQuote.Replace("u", " ");
+            movQuote = movQuote.Replace("a", " ");
+            Console.WriteLine(movQuote);
+            Console.WriteLine(" ");
+            Console.WriteLine("                   o           ");
+            Console.WriteLine("           o       /           ");
+            Console.WriteLine("            \\     /            ");
+            Console.WriteLine("             \\   /             ");
+            Console.WriteLine("              \\ /              ");
+            Console.WriteLine("+--------------v-------------+ ");
+            Console.WriteLine("|  __________________      @ | ");
+            Console.WriteLine("| /                  \\       | ");
+            Console.Write("| |             ,--, |  (\\)  | ");
+            Console.WriteLine(" __           __  ");
+            Console.Write("| |       _ ___/ /\\| |       | ");
+            Console.WriteLine("(  `.       ,`  ) ");
+            Console.Write("| |   ,;`( )__, )  ~ |  (-)  | ");
+            Console.WriteLine(" `.__\\_ _ _/__,'  ");
+            Console.Write("| |  // o//   '--;   |       | ");
+            Console.WriteLine("    ;(*) (*);     ");
+            Console.Write("| \\  ' o \\     |     / :|||: | ");
+            Console.WriteLine("    ;  \\ /  ;     ");
+            Console.Write("|  -ooo--------------  :|||: | ");
+            Console.WriteLine("     ;  o  ;      ");
+            Console.Write("+----------------------------+ ");
+            Console.WriteLine(" ,---'.`-','---.  ");
+            Console.Write("   []                    []    ");
+            Console.WriteLine(" |  |  `\"'  |  |  ");
+        }
+    }
+}
